c2102c5d62a50d9cc25d303e193d7af1abd438b9
Yigal <Yigal@localhost>
2013-11-24 22:45:42 +0200

    Adding VideoCombinedWriter for writing the output. Making VideoCapFile return (False, last_frame) upon last frame.

--- play.py
+++ play.py
@@ -1,16 +1,17 @@
 import rtmidi
+import time
 import sys
 import os
 import Queue
 import cv2
 
 from VideoOnOffTracker import VideoOnOffTracker
-from VideoCapFile import VideoCapCombiner
+from VideoCapFile import VideoCapCombiner, VideoCombinedWriter
 
 VERSION_FORMAT = '%(prog)s 1.0'
 
 videos_root_db = 'videos/'
-videos_dir = os.path.join(videos_root_db, 'eli_plays')
+videos_dir = os.path.join(videos_root_db, 'shenk_plays')
 
 winName = 'Display'
 cv2.namedWindow(winName)
@@ -19,18 +20,11 @@ class MidiInputCallback(object):
     '''Turns NoteOn and NoteOff midi events to (on_or_off, corresponding_note_video_path)
        messages on the message_queue.'''
     
-    def __init__(self, channels=None, do_prints=False):
+    def __init__(self, video_paths, channels=None, do_prints=False):
         self.do_prints = do_prints
         self.channels = channels
         self.message_queue = Queue.deque()
-        
-        self.video_paths = {}
-        for f in os.listdir(videos_dir):
-            note = int(os.path.splitext(f)[0])
-            # Videos are named with the note number only. Description can be put
-            # in the folder name.
-            vid_path = os.path.join(videos_dir, f)
-            self.video_paths[note] = vid_path
+        self.video_paths = video_paths
     
     def __call__(self, message_and_delta, data):
         message, time_delta = message_and_delta
@@ -59,11 +53,14 @@ class MidiInputCallback(object):
             self.message_queue.append((False, video_path))
         
         
-def run(message_queue):
+def run(args, message_queue, video_paths):
     'Finishes when exit_key is entered'
     exit_key = ord('q')
     on_off_tracker = VideoOnOffTracker()
     combiner = VideoCapCombiner()
+    
+    if args.out_vid:
+        writer = VideoCombinedWriter(video_paths)
     
     while True:
         currently_playing = on_off_tracker.process(message_queue)
@@ -71,6 +68,7 @@ def run(message_queue):
         ret, frame = combiner.read(currently_playing)
         if ret:
             cv2.imshow(winName, frame)
+            if args.out_vid: writer.write(frame)
    
         key = cv2.waitKey(38)
         # FIXME: A magic number if I ever saw one
@@ -78,6 +76,10 @@ def run(message_queue):
             cv2.destroyAllWindows()
             break
 
+    if args.out_vid: 
+        writer.release()
+        os.rename(writer._vidname, os.path.join('results', writer._vidname)) # VideoWriter can't get results/asd.avi
+        
 
 if __name__ == "__main__":
     import argparse
@@ -87,6 +89,7 @@ if __name__ == "__main__":
         'notes.')
     parser.add_argument('-i', '--midi_in', help="If not specified, the first port that looks like a loopback port is chosen")
     parser.add_argument('-v', '--verbose_midi_input', action='store_true')
+    parser.add_argument('-o', '--out_vid', action='store_true', help="If specified, video is being saved to results dir")
     parser.add_argument('-m', '--midi_channels', type=int, nargs='*', metavar='CHAN')
     parser.add_argument('--version', action='version', version=VERSION_FORMAT)
     args = parser.parse_args()
@@ -105,7 +108,16 @@ if __name__ == "__main__":
                 "To receive input from a keyboard, specify its name with --midi_in")
     midiin.open_port(in_port)
     print "MIDI received from %s" % (in_port_name, )
-    MidiInCb = MidiInputCallback(channels=args.midi_channels,
+    
+    video_paths = {}
+    for f in os.listdir(videos_dir):
+        note = int(os.path.splitext(f)[0])
+        # Videos are named with the note number only. Description can be put
+        # in the folder name.
+        vid_path = os.path.join(videos_dir, f)
+        video_paths[note] = vid_path
+
+    MidiInCb = MidiInputCallback(video_paths, channels=args.midi_channels,
                                  do_prints=args.verbose_midi_input)
     if args.midi_channels is not None:
         print "Playing channels %s" % (args.midi_channels, )
@@ -113,4 +125,5 @@ if __name__ == "__main__":
         print "Playing all channels"
     midiin.set_callback(MidiInCb)
     
-    run(MidiInCb.message_queue)
+    run(args, MidiInCb.message_queue, video_paths)
+
